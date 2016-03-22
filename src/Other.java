c8f471f9a3308eb0ef5ea001a7b6d58bcfdf641a
cwadley <clintwadley@gmail.com>
2016-03-22 17:19:28 -0400

    Added other optimization

--- src/Other.java
+++ src/Other.java
@@ -1,17 +1,27 @@
 public class Other {
 
-    public static String levelifier(int val, String toReturn) {
-    	if (val < 1500) {
-    	    return toReturn + Other.levelifier(++val, toReturn + "2");
-    	} else {
-    	    return "3";
-    	}
+    public static String levelifier() {
+        StringBuilder toReturn = new StringBuilder("Midnight");
+        StringBuilder addTo = new StringBuilder("Midnight");
+        for (int i = 0; i < 46; i++)
+        {
+            addTo.append("2");
+            toReturn.append(addTo.toString());
+        }
+        
+        return toReturn.toString();
     }
-
+    
     public static String threatLevel(int val) {
-    	String toReturn = "Midnight";
-    	String conv = Other.levelifier(val, toReturn).substring(0, Math.abs(val));
-    	return conv;
+        String conv = levelifier();
+        
+        if (val < 0)
+            conv = conv.substring(0, conv.length() - 2);
+        else
+            conv = conv.substring(0, conv.length() - 4);
+
+        conv = conv.substring(0, Math.abs(val));
+        return conv;
     }
 
     public static long defcon(long val) {
